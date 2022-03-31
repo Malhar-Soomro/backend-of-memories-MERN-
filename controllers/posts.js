196c1b4a65b38ce61e4d3f7ed773de148c678e43
Malhar-Soomro <malharsoomro2@gmail.com>
2022-03-31 08:27:01 +0500

    updatePost route added

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,12 +1,9 @@
+import mongoose from "mongoose";
 import PostMessage from "../models/postMessage.js";
 
 export const getPosts = async (req, res) => {
     try {
         const postMessage = await PostMessage.find();
-        // const postMsgs = {
-        //     msg: "hi there",
-        //     type: "String",
-        // }
         res.status(200).json(postMessage);
     }
     catch (err) {
@@ -23,6 +20,25 @@ export const createPost = async (req, res) => {
         res.status(201).json(newPost);
     } catch (error) {
         res.status(409).json({ message: error.message })
+    }
+
+}
+
+export const updatePost = async (req, res) => {
+    const { id: _id } = req.params;
+    const updatedPost = req.body;
+
+    //is the post with this id present or not
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).send("no post with this id");
+    }
+
+    try {
+        const newPost = await PostMessage.findByIdAndUpdate(_id, updatedPost, { new: true });
+        return res.status(200).json(newPost);
+    }
+    catch (error) {
+        return res.status(404).send(error.message);
     }
 
 }
