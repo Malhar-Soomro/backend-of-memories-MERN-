94330e02f2741fb047955f858d01f8be5c1a0e3d
Malhar-Soomro <malharsoomro2@gmail.com>
2022-04-04 15:40:12 +0500

    deploying to heroku

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import postRouter from "./routes/posts.js";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config()
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -17,13 +19,13 @@ app.get("/", (req, res) => {
 });
 
 
-const MONGO_URI = "mongodb://malhar:malhar1122@cluster0-shard-00-00.0mdmt.mongodb.net:27017,cluster0-shard-00-01.0mdmt.mongodb.net:27017,cluster0-shard-00-02.0mdmt.mongodb.net:27017/Memories?ssl=true&replicaSet=atlas-de05qz-shard-0&authSource=admin&retryWrites=true&w=majority"
+// const MONGO_URI = "mongodb://malhar:malhar1122@cluster0-shard-00-00.0mdmt.mongodb.net:27017,cluster0-shard-00-01.0mdmt.mongodb.net:27017,cluster0-shard-00-02.0mdmt.mongodb.net:27017/Memories?ssl=true&replicaSet=atlas-de05qz-shard-0&authSource=admin&retryWrites=true&w=majority"
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, console.log(`server is running on port: ${PORT}`)))
-    .catch((err) => console.log(err.message));
+    .catch((err) => console.log(err));
 
 // mongoose.set("useFindAndModify", false);
 
